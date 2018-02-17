442aa2c8b4490657f3e2579b0cc83b01147b1237
duforetn <nduforet@gmail.com>
2018-02-17 17:32:40 +0100

    use get_json_object

--- src/main/scala/RegressionApp.scala
+++ src/main/scala/RegressionApp.scala
@@ -21,34 +21,29 @@ object StreamingRegression extends SparkApp with StrictLogging {
   .option("subscribe", conf.topic)
   .load()
 
-  val newData = new StructType()
-    .add("y", DoubleType)
-    .add("var1", DoubleType)
-    .add("var2", DoubleType)
-
-  val yCol = new StructType()
-    .add("y", DoubleType)
-
-  val featureCol = new StructType()
-    .add("var1", DoubleType)
-    .add("var2", DoubleType)
-
-  val df1 = df.withColumn("newData", from_json($"value".cast(StringType), newData))
-              .withColumn("label", from_json($"value".cast(StringType), yCol))
-              .withColumn("features", from_json($"value".cast(StringType), featureCol))
-              .select("newData", "label", "features")
+  val df1 = df.select(
+    get_json_object($"value".cast("string"), "$.y").alias("y").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var1").alias("var1").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var2").alias("var2").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var3").alias("var3").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var4").alias("var4").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var5").alias("var5").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var6").alias("var6").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var7").alias("var7").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var8").alias("var8").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var9").alias("var9").cast(DoubleType),
+    get_json_object($"value".cast("string"), "$.var10").alias("var10").cast(DoubleType)
+  )
 
   val streamDebug = df1.writeStream
     .format("console")
     .queryName("debug")
     .start()
-
   val mlStreaming = df1.writeStream
     .format("streaming.RegressionSinkProvider")
     .outputMode("update")
     .queryName("RegressionStreaming")
     .start()
-
   //Wait for all streams to finish
   spark.streams.awaitAnyTermination()
 }
